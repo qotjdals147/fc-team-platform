b2e87b7e139358cbde1d4809b146d49c2f767f1d
qotjdals147 <qotjdals147@gmail.com>
2026-06-17 17:14:03 +0900

    Add files via upload

--- js/config.js
+++ js/config.js
@@ -3,8 +3,9 @@ export const PLATFORM = {
   name: 'FC 플랫폼',
   version: '0.1.0',
   /** 별도 Supabase 프로젝트 착수 시 채움 */
-  SUPABASE_URL: '',
-  SUPABASE_KEY: '',
+  SUPABASE_URL: 'https://rdscgnvseplwlftjixom.supabase.co',
+  SUPABASE_KEY:
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJkc2NnbnZzZXBsd2xmdGppeG9tIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE2NTQ0ODUsImV4cCI6MjA5NzIzMDQ4NX0.N6NQH06izI-4g832jsmmBu-6vqtc9mnwnHAHSfRwkSQ',
   /** 베타·Pro 무광고 시 false */
   adsEnabled: true,
   /** 개발: 광고 슬롯 회색 박스 표시 */
